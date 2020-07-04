6989bc4dcd29689b101f6a0dfda3d09361a00010
lauraschoeneberg <65074525+lauraschoeneberg@users.noreply.github.com>
2020-07-04 12:00:51 +0200

    resize columns to contents

--- src/view/mzTabTableWidget.py
+++ src/view/mzTabTableWidget.py
@@ -3,19 +3,18 @@ mzTabTableWidget
 ----------------
 This script allows the user to transfer information about proteins and psms from a mzTab file into two tables, 
 one containing the proteins, the other one containing the psms.
-
 By clicking on a row, the tables get updated regarding their listed proteins or psms.
 Once you choose a protein/psm, the table displays only those psms/proteins that are linked to one another.
-
 This tool is designed to accept mzTab files. It is required to save those files under '.../examples/data/' or 
 change the path within the InitWindow.
 """
 import sys
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QVBoxLayout, QTableWidgetItem
+import webbrowser
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QVBoxLayout, QTableWidgetItem, QPushButton, QFileDialog
 
 
-class mzTabTableWidget(QWidget):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
 
@@ -24,14 +23,32 @@ class mzTabTableWidget(QWidget):
         self.left = 100
         self.width = 500
         self.height = 500
+        self.tableRows = 5
 
-        self.PRT = []
-        self.PSM = []
+        self.fileLoaded = False
 
-        self.drawPSM = []
-        self.drawPRT = []
+        self.PRTFull = []
+        self.PSMFull = []
 
-        self.selected = ""
+        self.PRTFiltered = []
+        self.PSMFiltered = []
+
+        self.PRTColumn = [True]
+        self.PSMColumn = [True]
+
+        self.selectedPRT = ""
+        self.selectedPSM = ""
+
+        self.tablePRTFull = QTableWidget()
+        self.tablePSMFull = QTableWidget()
+
+        self.tablePRTFiltered = QTableWidget()
+        self.tablePSMFiltered = QTableWidget()
+
+        self.vBoxPRT = QVBoxLayout()
+        self.vBoxPSM = QVBoxLayout()
+
+        self.outerVBox = QVBoxLayout()
 
         self.InitWindow()
 
@@ -40,25 +57,80 @@ class mzTabTableWidget(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
 
-        self.parser('../examples/data/iPRG2015.mzTab')
+        self.tablePRTFull.setHidden(True)
+        self.tablePSMFull.setHidden(True)
+        self.tablePRTFiltered.setHidden(True)
+        self.tablePSMFiltered.setHidden(True)
 
-        self.drawPRT = self.PRT
-        self.drawPSM = self.PSM
+        self.loadButton = QtWidgets.QPushButton(self)
+        self.loadButton.setText("load")
+        self.loadButton.clicked.connect(self.loadFile)
 
-        self.tableWidget1 = QTableWidget()
-        self.tableWidget2 = QTableWidget()
-        self.drawTables()
-        self.createProtTable()
-        self.createPSMTable()
+        self.tablePRTFull.itemClicked.connect(self.PRTClicked)
+        self.tablePRTFiltered.itemClicked.connect(self.PRTClicked)
+        self.tablePSMFull.itemClicked.connect(self.PSMClicked)
+        self.tablePSMFiltered.itemClicked.connect(self.PSMClicked)
 
-        self.tableWidget1.itemClicked.connect(self.filterProteins)
-        self.tableWidget2.itemClicked.connect(self.filterPSMs)
+        self.tablePRTFull.itemDoubleClicked.connect(self.browsePRT)
+        self.tablePRTFiltered.itemDoubleClicked.connect(self.browsePRT)
+        self.tablePSMFull.itemDoubleClicked.connect(self.browsePSM)
+        self.tablePSMFiltered.itemDoubleClicked.connect(self.browsePSM)
 
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.tableWidget1)
-        self.vbox.addWidget(self.tableWidget2)
-        self.setLayout(self.vbox)
+        self.vBoxPRT.addWidget(self.tablePRTFull)
+        self.vBoxPRT.addWidget(self.tablePRTFiltered)
+
+        self.vBoxPSM.addWidget(self.tablePSMFull)
+        self.vBoxPSM.addWidget(self.tablePSMFiltered)
+
+        self.outerVBox.addLayout(self.vBoxPRT)
+        self.outerVBox.addLayout(self.vBoxPSM)
+
+        self.setLayout(self.outerVBox)
         self.show()
+
+    def loadFile(self):
+        if self.fileLoaded:
+            self.tablePRTFull.clear()
+            self.tablePSMFull.clear()
+            self.tablePSMFiltered.clear()
+            self.tablePRTFiltered.clear()
+
+            self.tablePRTFull.setRowCount(0)
+            self.tablePSMFull.setRowCount(0)
+            self.tablePSMFiltered.setRowCount(0)
+            self.tablePRTFiltered.setRowCount(0)
+
+            self.PRTFull.clear()
+            self.PSMFull.clear()
+
+            self.PRTFiltered.clear()
+            self.PSMFiltered.clear()
+
+            self.PRTColumn.clear()
+            self.PSMColumn.clear()
+
+            self.PRTColumn = [True]
+            self.PSMColumn = [True]
+
+        self.filename = QFileDialog.getOpenFileName()
+
+        self.parser(self.filename[0])
+
+        self.PRTColumn *= len(self.PRTFull[1])
+        self.PSMColumn *= len(self.PSMFull[1])
+
+        self.initTables()
+        self.createTable(self.tablePRTFull, self.PRTFull)
+        self.createTable(self.tablePSMFull, self.PSMFull)
+
+        self.hidePRTColumns()
+        self.hidePSMColumns()
+
+        self.tablePRTFull.setHidden(False)
+        self.tablePSMFull.setHidden(False)
+
+        self.fileLoaded = True
+
 
     def parser(self, file):
         """parses the given mzTab file and saves PRT and PSM information
@@ -71,73 +143,181 @@ class mzTabTableWidget(QWidget):
         with open(file) as inp:
             for line in inp:
                 if line.startswith("PRH"):
-                    self.PRT.append(line.strip().split('\t'))
+                    self.PRTFull.append(line.strip().split('\t'))
                 elif line.startswith("PRT") and not line.endswith("protein_details\n"):
-                    self.PRT.append(line.strip().split('\t'))
+                    self.PRTFull.append(line.strip().split('\t'))
                 elif line.startswith("PSH") or line.startswith("PSM"):
-                    self.PSM.append(line.strip().split('\t'))
+                    self.PSMFull.append(line.strip().split('\t'))
 
-        for item in self.PRT:
+        for item in self.PRTFull:
             item.pop(0)
 
-        for item in self.PSM:
+        for item in self.PSMFull:
             item.pop(0)
 
-    def drawTables(self):
-        """draws protein and psm table with headers"""
 
-        self.tableWidget1.setRowCount(len(self.drawPRT)-1)
-        self.tableWidget1.setColumnCount(len(self.drawPRT[0]))
-        self.tableWidget1.setHorizontalHeaderLabels(self.drawPRT[0])
 
-        self.tableWidget2.setRowCount(len(self.drawPSM)-1)
-        self.tableWidget2.setColumnCount(len(self.drawPSM[0]))
-        self.tableWidget2.setHorizontalHeaderLabels(self.drawPSM[0])
-        
-    def createProtTable(self):
-        """updates protein table by setting table items and changing number of rows"""
-        self.tableWidget1.setRowCount(len(self.drawPRT)-1)
+    def initTables(self):
+        """draws protein and psm tables with headers"""
+
+        self.tablePRTFull.setRowCount(len(self.PRTFull))
+        self.tablePRTFull.setColumnCount(len(self.PRTFull[0]))
+        self.tablePRTFull.setHorizontalHeaderLabels(self.PRTFull[0])
+
+        self.tablePSMFull.setRowCount(len(self.PSMFull))
+        self.tablePSMFull.setColumnCount(len(self.PSMFull[0]))
+        self.tablePSMFull.setHorizontalHeaderLabels(self.PSMFull[0])
+
+        self.tablePRTFiltered.setRowCount(0)
+        self.tablePRTFiltered.setColumnCount(len(self.PRTFull[0]))
+        self.tablePRTFiltered.setHorizontalHeaderLabels(self.PRTFull[0])
+
+        self.tablePSMFiltered.setRowCount(0)
+        self.tablePSMFiltered.setColumnCount(len(self.PSMFull[0]))
+        self.tablePSMFiltered.setHorizontalHeaderLabels(self.PSMFull[0])
+
+        """removes now unnecessary header information from content lists """
+        self.PRTFull.pop(0)
+        self.PSMFull.pop(0)
+
+    def createTable(self, table, content):
+        """parameters: tableWidget to draw content in. Content to be drawn in list form"""
+        """Setting count to zero empties the table. Then table is (re-)filled with specified content"""
+        table.setRowCount(0)
+        table.setRowCount(len(content))
 
         j = 0
         k = 0
 
-        for item in self.drawPRT[1:]:
-            while k < (len(self.drawPRT)-1):
+        for item in content[0:]:
+            while k < (len(content)):
                 while j < (len(item)):
-                    self.tableWidget1.setItem(k, j, QTableWidgetItem(item[j]))
+                    table.setItem(k, j, QTableWidgetItem(item[j]))
                     j += 1
                 else:
                     k += 1
                     j = 0
                 break
+        self.tablePRTFull.resizeColumnsToContents() # resize columns
+        self.tablePSMFull.resizeColumnsToContents() # resize columns
+        self.tablePRTFiltered.resizeColumnsToContents() # resize columns
+        self.tablePSMFiltered.resizeColumnsToContents() # resize columns
+    def hidePRTColumns(self):
+        """hides constant columns in PRT table by default by checking if every value equals"""
+        i = 0
+        j = 0
+        k = 0
 
-    def createPSMTable(self):
-        """updates psm table by setting table items and changing number of rows"""
-        self.tableWidget2.setRowCount(len(self.drawPSM)-1)
+        while i < len(self.PRTFull) - 1:
+            while j < len(self.PRTFull[i]):
+                if self.PRTColumn[j]:
+                    if self.PRTFull[i][j] != self.PRTFull[i + 1][j]:
+                        self.PRTColumn[j] = False
+                j += 1
+            i += 1
 
-        m = 0
-        n = 0
+        while k < len(self.PRTColumn):
+            if self.PRTColumn[k]:
+                self.tablePRTFull.setColumnHidden(k, True)
+                self.tablePRTFiltered.setColumnHidden(k, True)
+            k += 1
 
-        for item in self.drawPSM[1:]:
-            while n < (len(self.drawPSM)-1):
-                while m < (len(item)):
-                    self.tableWidget2.setItem(n, m, QTableWidgetItem(item[m]))
-                    m += 1
-                else:
-                    n += 1
-                    m = 0
-                break
+    def hidePSMColumns(self):
+        """hides constant columns in PSM table by default by checking if every value equals"""
+        i = 0
+        j = 0
+        k = 0
 
-    def filterProteins(self, item):
-        self.selected = self.PRT[item.row()+1][0]
-        print(self.selected)
-        self.drawPSM = [p for p in self.PSM if p[2] == self.selected]
-        self.createPSMTable()
+        while i < len(self.PSMFull) - 1:
+            while j < len(self.PSMFull[i]):
+                if self.PSMColumn[j]:
+                    if self.PSMFull[i][j] != self.PSMFull[i + 1][j]:
+                        self.PSMColumn[j] = False
+                j += 1
+            i += 1
 
-    def filterPSMs(self, item):
-        self.selected = self.PSM[item.row()+1][2]
-        print(self.selected)
-        self.drawPSM = [f for f in self.PSM if f[0] == self.selected]
-        self.createProtTable()
+        while k < len(self.PSMColumn):
+            if self.PSMColumn[k]:
+                self.tablePSMFull.setColumnHidden(k, True)
+                self.tablePSMFiltered.setColumnHidden(k, True)
+            k += 1
 
+    def PRTClicked(self, item):
 
+        if self.tablePRTFull.isHidden():
+            relevantContent = self.PRTFiltered
+        else:
+            relevantContent = self.PRTFull
+
+        accession = relevantContent[item.row()][0]
+
+        if self.selectedPSM == accession:
+            self.unfilterPSM()
+        else:
+            self.filterPSM(accession)
+
+    def PSMClicked(self, item):
+
+        if self.tablePSMFull.isHidden():
+            relevantContent = self.PSMFiltered
+        else:
+            relevantContent = self.PSMFull
+
+        accession = relevantContent[item.row()][2]
+
+        if self.selectedPRT == accession:
+            self.unfilterPRT()
+        else:
+            self.filterPRT(accession)
+
+    def filterPRT(self, accession):
+        self.tablePRTFiltered.setHidden(False)
+        self.tablePRTFull.setHidden(True)
+
+        self.selectedPRT = accession
+
+        self.PRTFiltered = [p for p in self.PRTFull if p[0] == self.selectedPRT]
+        self.createTable(self.tablePRTFiltered, self.PRTFiltered)
+
+    def filterPSM(self, accession):
+        self.tablePSMFiltered.setHidden(False)
+        self.tablePSMFull.setHidden(True)
+
+        self.selectedPSM = accession
+
+        self.PSMFiltered = [p for p in self.PSMFull if p[2] == self.selectedPSM]
+        self.createTable(self.tablePSMFiltered, self.PSMFiltered)
+
+    def unfilterPRT(self):
+        self.tablePRTFiltered.setHidden(True)
+        self.tablePRTFull.setHidden(False)
+        self.selectedPRT = ""
+        self.PRTFiltered = []
+
+    def unfilterPSM(self):
+        self.tablePSMFiltered.setHidden(True)
+        self.tablePSMFull.setHidden(False)
+        self.selectedPSM = ""
+        self.PSMFiltered = []
+
+    def browsePRT(self, item):
+        if self.tablePRTFull.isHidden():
+            accession = self.PRTFiltered[item.row()][0].split("|", 2)[1]
+        else:
+            accession = self.PRTFull[item.row()][0].split("|", 2)[1]
+
+        webbrowser.open("https://www.uniprot.org/uniprot/" + accession)
+
+    def browsePSM(self, item):
+        if self.tablePSMFull.isHidden():
+            accession = self.PSMFiltered[item.row()][2].split("|", 2)[1]
+        else:
+            accession = self.PSMFull[item.row()][2].split("|", 2)[1]
+
+        webbrowser.open("https://www.uniprot.org/uniprot/" + accession)
+
+"""
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
+"""

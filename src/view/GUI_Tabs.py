c4e63ff48aadd101363e8b84a64729f24eaff0b5
lauraschoeneberg <65074525+lauraschoeneberg@users.noreply.github.com>
2020-07-20 00:22:29 +0200

    fixed bug regarding popup window

--- src/view/GUI_Tabs.py
+++ src/view/GUI_Tabs.py
@@ -1,15 +1,20 @@
+import os
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QMessageBox, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, \
+    QMessageBox, QFileDialog
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+import glob
+from filehandler import FileHandler
+from tableDataFrame import TableDataFrame
 
 from mzTabTableWidget import mzTabTableWidget
 from GUI_FastaViewer import Window
-from SpectrumWidget import SpectrumWidget
 from ErrorWidget import ErrorWidget
+
 sys.path.insert(0, '../apps')
 from SpecViewer import App
-from XMLViewer import XMLViewer
+from ConfigView import ConfigView
 from mzMLTableView import mzMLTableView
 
 
@@ -24,46 +29,51 @@ class AppGUITabs(QMainWindow):
         self.height = 200
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        #setTab3()
+        # setTab3()
         self.table_widget = MyTableWidget(self)
         self.setCentralWidget(self.table_widget)
-
+        
         self.show()
 
-
-        
 
 class MyTableWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
 
-        self.initButton = QPushButton(self) #neu1
-        self.initButton.setText("Load Data")
-        self.initButton.clicked.connect(self.show_popup) #neu1
+        self.loadedFolder = ""
+        self.loadedFasta = ""
+        self.loadedIni = ""
+        self.loadedTsv = ""
+
+        self.AutoLoadedData = False #neu
         
-        self.loadButton = QPushButton(self) #neu
+        self.initButton = QPushButton(self)  # neu1
+        self.initButton.setText("Load Data")
+        self.initButton.clicked.connect(self.show_popup)  # neu1
+
+        self.loadButton = QPushButton(self)  # neu
         self.loadButton.setText("Run ProteomicsLFQ")
         self.loadButton.clicked.connect(self.LFQ)
 
-        #initialize tab screen
+        # initialize tab screen
         self.tabs = QTabWidget()
-        self.tab1 = XMLViewer()
+        self.tab1 = ConfigView()
         self.tab2 = mzMLTableView()
         self.tab3 = mzTabTableWidget()
         self.tab4 = Window()
         self.tab5 = App()
 
-        self.tabs.resize(300,200)
+        self.tabs.resize(300, 200)
 
-        #add tabs
+        # add tabs
         self.tabs.addTab(self.tab1, "Ini-Config")
         self.tabs.addTab(self.tab2, "Experimental Design")
         self.tabs.addTab(self.tab3, "PSM/PRT Table")
         self.tabs.addTab(self.tab4, "Fasta-Viewer")
         self.tabs.addTab(self.tab5, "Spectrum Viewer")
 
-        #add tabs to widget
+        # add tabs to widget
         self.layout.addWidget(self.initButton)
         self.layout.addWidget(self.loadButton)
         self.layout.addWidget(self.tabs)
@@ -74,33 +84,82 @@ class MyTableWidget(QWidget):
         msg.setWindowTitle("Attention!")
         msg.setText("Want data to be loaded automatically?  ")
         msg.setIcon(QMessageBox.Question)
-        msg.setDetailedText("If you choose to load data automatically, please enter the file that contains entire data (for different widgets). Otherwise, please enter data manually through widgets. Thank you.")
-        msg.setStandardButtons(QMessageBox.Yes|QMessageBox.Cancel)
+        msg.setDetailedText(
+            "If you choose to load data automatically, please enter the folder that contains entire data (for different widgets). Otherwise, please enter data manually through widgets. Thank you.")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.Cancel)
         msg.buttonClicked.connect(self.popupbutton_clicked)
-                
+
         x = msg.exec_()
 
-    def popupbutton_clicked(self,i):
-        #i is either 'Yes' or 'Cancel'
-        if not (i == QMessageBox.Cancel):
-          options = QFileDialog.Options()
-          file, _ = QFileDialog.getOpenFileName(
-              self, "QFileDialog.getOpenFileName()", "", "All Files (*);;tsv (*.tsv);;csv (*.csv);;fasta (*.fasta);; mzML (*.mzML);; ini (*.ini)", options=options)
-          
-            
+    def popupbutton_clicked(self, i):
+        # i is either 'Yes' or 'Cancel'
+        if i.text() == "&Yes":
+            self.AutoLoadedData = True #neu
+            dialog = QFileDialog(self)
+            self.loadedFolder = dialog.getExistingDirectory()
+            print(self.loadedFolder)
+
+            os.chdir(self.loadedFolder)
+            for file in glob.glob("*.fasta"):
+                self.tab4.loadFile(file)
+                self.loadedFasta = file
+
+            for file in glob.glob("*.ini"):
+                self.tab1.generateTreeWidgetItem(file)
+                self.loadedIni = file
+
+            for file in glob.glob("*.tsv"):
+                TableDataFrame.setTable(self.tab2, FileHandler.importTable(self.tab2, file))
+                self.tab2.drawTable()
+                self.loadedTsv = file
+
+            if self.loadedTsv == "":
+                self.tab2.loadDir(self.loadedFolder)
+
     def LFQ(self):
+        if not self.AutoLoadedData:
+            """self.loadedTsv = 
+            self.loadedFasta = 
+            self.loadedIni = """
+            
+        os.chdir(self.loadedFolder)
+        mzML = glob.glob("*.mzML")
+        idXML = glob.glob("*.idXML")
 
-        fastapath = self.tab4.path
-        tsvpath = self.tab2.path
+        if len(mzML) == len(idXML):
 
-        print("path to fasta = " + fastapath)
-        print("path to tsv = " + tsvpath)
+            command = "ProteomicsLFQ -in    "
 
-        if (fastapath != "") & (tsvpath != ""):
-            self.tab3.readFile("../examples/data/iPRG2015.mzTab")
+            for file in mzML:
+                command += file + " "
+
+            command += "-ids "
+
+            for file in idXML:
+                command += file + " "
+
+            command += "-design " + self.loadedTsv + " "
+            command += "-fasta " + self.loadedFasta + " "
+            command += "-Alignment:max_rt_shift 0 " \
+                       "-targeted_only true " \
+                       "-transfer_ids false " \
+                       "-mass_recalibration false " \
+                       "-out_cxml BSA.consensusXML.tmp " \
+                       "-out_msstats BSA.csv.tmp " \
+                       "-out BSA.mzTab.tmp " \
+                       "-threads 1 " \
+                       "-proteinFDR 0.3"
+
+            print(command)
+            os.chdir(self.loadedFolder)
+            os.system(command)
+
+            for file in glob.glob("*.mzTab"):
+                self.tab3.readFile(file)
 
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = AppGUITabs()
     sys.exit(app.exec_())
